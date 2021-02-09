ac92955c8e2947d161a8cd059c750fa6e3c85b4f
jorgechauxjr <chauxjorge@hotmail.com>
2021-02-09 18:20:22 -0500

    Update main.ts task4 writing a class

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -44,3 +44,28 @@ export const printTeacher: printTeacherFunction = (firstName: string, lastName: 
 
 //example
 console.log(printTeacher("John", "Doe"));
+
+// TASK 4
+// https://www.typescriptlang.org/docs/handbook/interfaces.html
+// Difference between the static and instance sides of classes
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentInterface;
+}
+interface StudentInterface {
+  firstName: string;
+  lastName: string;
+}
+class StudentClass implements StudentInterface{
+  firstName: string;
+  lastName: string;
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  workOnHomework() {
+    return "Currently working";
+  }
+  displayName() {
+    this.firstName;
+  }
+}
