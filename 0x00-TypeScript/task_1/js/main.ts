af6f2f67c68a8aa57a53afb630758d33944fef59
jorgechauxjr <chauxjorge@hotmail.com>
2021-02-09 15:58:19 -0500

    Update main.ts task2 director interface

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -17,3 +17,18 @@ const teacher3: Teacher = {
 };
 
 console.log(teacher3);
+
+// task 2 Director Interface
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+// ex creating Director
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+console.log(director1);
