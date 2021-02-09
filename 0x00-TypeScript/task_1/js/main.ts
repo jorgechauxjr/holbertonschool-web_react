592cccff021f4783846a698f5b0f164c0f4ad859
jorgechauxjr <chauxjorge@hotmail.com>
2021-02-09 13:17:43 -0500

    del comments tk1 main.ts

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -1,13 +1,9 @@
 // task_1 Teacher Interface
-
 interface Teacher {
-  // readonly because first and last name
-  // should only be modifiable when a Teacher is first initialized
   readonly firstName: string;
   readonly lastName: string;
   fullTimeEmployee: boolean;
-  location: string;
-  //  ? at the end of the property means optional propperty
   yearsOfExperience?: number;
+  location: string;
   [customAttribute: string]: any; 
 }
