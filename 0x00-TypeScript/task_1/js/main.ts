3f1b065ed3885dbfbcb9722272900a3502032cd5
jorgechauxjr <chauxjorge@hotmail.com>
2021-02-09 12:59:52 -0500

    Try tk2 main.ts

---
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -0,0 +1,23 @@
+// task_1 Teacher Interface
+
+interface Teacher {
+  // readonly because first and last name
+  // should only be modifiable when a Teacher is first initialized
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  location: string;
+  //  ? at the end of the property means optional propperty
+  yearsOfExperience?: number;
+  [customAttribute: string]: any; 
+}
+
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher3);
