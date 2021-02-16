1bb0b8de33c1af351ce5d76abb04277907599d2d
jorgechauxjr <chauxjorge@hotmail.com>
2021-02-16 15:57:40 -0500

    Update add lael and input App.js task_2

--- 0x02-react_intro/task_2/dashboard/src/App.js
+++ 0x02-react_intro/task_2/dashboard/src/App.js
@@ -11,6 +11,12 @@ function App() {
       </header>
       <body className='App-body'>
         <p>Login to access the full dashboard</p>
+        {/* https://reactjs.org/docs/dom-elements.html */}
+        <label htmlFor="email">Email: </label>
+        <input type="email" id="email" />
+        <label htmlFor="psw">Password: </label>
+        <input type="password" id="psw" />
+        <button>OK</button>
       </body>
       <footer className='App-footer'>
       <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
