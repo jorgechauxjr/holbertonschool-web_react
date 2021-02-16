c73437019b3f2539cee099edfbc67e28db4cd6ba
jorgechauxjr <chauxjorge@hotmail.com>
2021-02-16 15:14:50 -0500

    Modify src/App.js

--- 0x02-react_intro/task_1/dashboard/src/App.js
+++ 0x02-react_intro/task_1/dashboard/src/App.js
@@ -1,20 +1,20 @@
 import logo from './holberton-logo.jpg';
 import './App.css';
+import { getFullYear, getFooterCopy } from './utils';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <h1>School dashboard</h1>
       </header>
-      
-      <body className="App-body">
+      <body className='App-body'>
         <p>Login to access the full dashboard</p>
       </body>
-
-      <footer className="App-footer">
-        <p>Copyright 2020 - holberton School</p>
+      <footer className='App-footer'>
+      <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      {/* <p>{`Copyright ${getFullYear()} - ${getFooterCopy(true)}`}</p> */}
       </footer>
     </div>
   );
