f6e8ba22ee853d583a422594a19b8a00816fedc9
jorgechauxjr <chauxjorge@hotmail.com>
2021-02-16 16:47:01 -0500

    Updated utils.js task_2 3.Modify the notifications

--- 0x02-react_intro/task_2/dashboard/src/utils.js
+++ 0x02-react_intro/task_2/dashboard/src/utils.js
@@ -9,3 +9,7 @@ export function getFooterCopy(isIndex) {
   if (isIndex) return "Holberton School"
   return "Holberton School main dashboard"
 }
+
+export function getLatestNotification() {
+  return "<strong>Urgent requirement</strong> - complete by EOD";
+}
