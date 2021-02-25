a6babefff7d904444b7c2a1a000770dc92e0d9d3
jorgechauxjr <chauxjorge@hotmail.com>
2021-02-25 14:37:02 -0500

    Add task_1 tests for header, login, footer and app

--- 0x03-react_props/task_1/dashboard/src/App/App.test.js
+++ 0x03-react_props/task_1/dashboard/src/App/App.test.js
@@ -1,11 +1,35 @@
-import React from 'react';
+/*
+    test that App renders without crashing
+    verify that App renders a div with the class App-header
+    verify that App renders a div with the class App-body
+    verify that App renders a div with the class App-footer
+*/
 import { shallow } from 'enzyme';
-import { expect } from 'chai';
+import React from "react";
 import App from './App';
+import Notifications from '../Notifications/Notifications';
+import Login from '../Login/Login';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 
-describe('Test App.js', () => {
-  it('App without crashing', (done) => {
-    expect(shallow(<App />).exists());
-    done();
-  });
-});
+
+describe("test App", () => {
+    it("renders without crashing", () => {
+        shallow(<App />);
+    });
+    const wrapper = shallow(<App />);
+    it("Notifications", () => {
+        shallow(<Notifications />);
+        
+    });
+    it("Header", () => {
+        shallow(<Header />);
+    });
+    it("Login", () => {
+        shallow(<Login />);
+    });
+    it("Footer", () => {
+        shallow(<Footer />);
+    });
+}); 
+  
