ed01042d9957cb2324cd462a3c3197984dd12a19
jorgechauxjr <chauxjorge@hotmail.com>
2021-02-25 15:08:30 -0500

    Modify task_2 Notifications.js and Notifications.test.js

--- 0x03-react_props/task_2/dashboard/src/Notifications/Notifications.js
+++ 0x03-react_props/task_2/dashboard/src/Notifications/Notifications.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Notifications.css';
 import close_icon from '../assets/close-icon.png';
 import { getLatestNotification } from '../utils/utils';
+import NotificationItem from './NotificationItem'
 
 const Notifications = () => {
   const btnStyle = {
@@ -21,9 +22,9 @@ const Notifications = () => {
     <div className="Notifications">
       <p>Here is the list of notifications</p>
       <ul>
-        <li data-priority='default'>New course available</li>
-        <li data-priority='urgent'>New resume available</li>
-        <li data-priority='urgent' dangerouslySetInnerHTML={{__html: getLatestNotification()}}></li>
+        <NotificationItem type='default' value='New course available'></NotificationItem>
+        <NotificationItem type='urgent' value='New resume available'></NotificationItem>
+        <NotificationItem type='urgent' html={{ __html: getLatestNotification() }}></NotificationItem>
       </ul>
       <button style={btnStyle} aria-label='Close' onClick={() => console.log('Close button has been clicked')}>
         <img src={close_icon} style={imgStyle}/>
--- 0x03-react_props/task_2/dashboard/src/Notifications/Notifications.test.js
+++ 0x03-react_props/task_2/dashboard/src/Notifications/Notifications.test.js
@@ -1,24 +1,20 @@
-import React from 'react';
 import { shallow } from 'enzyme';
-import { expect } from 'chai';
+import React from "react";
 import Notifications from './Notifications';
 
-describe('Test Notification.js', () => {
-  it('Notification without crashing', (done) => {
-    expect(shallow(<Notifications />).exists());
-    done();
-  });
 
-  it('renders three list items', (done) => {
+describe("Test Suite Notificacions", () => {
+    //test that Notifications renders without crashing
+    it("renders without crashing", () => {
+        const wrapper = shallow(<Notifications />);
+        shallow(<Notifications />);
+        expect(wrapper.exists());
+    });
     const wrapper = shallow(<Notifications />);
-    expect(wrapper.find('ul')).to.have.lengthOf(1);
-    expect(wrapper.find('li')).to.have.lengthOf(3);
-    done();
-  });
-
-  it(' renders the text "Here is the list of notifications"', (done) => {
-    const wrapper = shallow(<Notifications />);
-    expect(wrapper.contains(<body className='App-body' />))
-    done();
-  });
-});
+    it("verify that Notifications renders three list items", () => {
+        expect(wrapper.find('ul').children().length).toEqual(3);
+    });
+    it("verify that Notifications renders the text Here is the list of notifications", () => {
+        expect(wrapper.containsMatchingElement(<p>Here is the list of notifications</p>)).toEqual(true);
+    });
+}); 
