d29a3caf8e6785904f76d6abd01e579421d930fe
jorgechauxjr <chauxjorge@hotmail.com>
2021-02-26 17:15:43 -0500

    Modify App.js create a new array  named listCourses

--- 0x03-react_props/task_5/dashboard/src/App/App.js
+++ 0x03-react_props/task_5/dashboard/src/App/App.js
@@ -7,13 +7,19 @@ import Footer from '../Footer/Footer'
 import PropTypes from 'prop-types'; // ES6
 
 function App({ isLoggedIn }) {
+  const listCourses = [
+    { id: 1, name: 'ES6', credit: 60 },
+    { id: 2, name: 'Webpack', credit: 20 },
+    { id: 3, name: 'React', credit: 40 }
+  ];
+
   return (
     <React.Fragment>
       <Notifications></Notifications>
       <div className='App'>
         <Header></Header>
         {/* operador ternario */}
-        {isLoggedIn ? <CourseList></CourseList> : <Login></Login>}
+        {isLoggedIn ? <CourseList listCourses={listCourses} ></CourseList> : <Login></Login>}
         <Footer></Footer>
       </div>
     </React.Fragment>
