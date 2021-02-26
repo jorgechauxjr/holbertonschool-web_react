9e31a5c4893f9b3380d87bf8c11bcf53fc6304e7
jorgechauxjr <chauxjorge@hotmail.com>
2021-02-26 17:05:38 -0500

    Modify CourseList.js task_5

--- 0x03-react_props/task_5/dashboard/src/CourseList/CourseList.js
+++ 0x03-react_props/task_5/dashboard/src/CourseList/CourseList.js
@@ -1,22 +1,41 @@
 import React from 'react';
 import CourseListRow from './CourseListRow';
 import './CourseList.css';
+import PropTypes from 'prop-types'; // ES6
+import CourseShape from './CourseShape';
 
 
-function CourseList() {
+function CourseList({ listCourses }) {
   return (
-    <table id="CourseList">
-      <thead>
-        <CourseListRow textFirstCell="Available courses" isHeader={true}></CourseListRow>
-        <CourseListRow textFirstCell="Course name" textSecondCell="Credit" isHeader={true}></CourseListRow>
-      </thead>
-      <tbody>
-        <CourseListRow textFirstCell="ES6" textSecondCell="60" isHeader={false}></CourseListRow>
-        <CourseListRow textFirstCell="Webpack" textSecondCell="20" isHeader={false}></CourseListRow>
-        <CourseListRow textFirstCell="React" textSecondCell="40" isHeader={false}></CourseListRow>
+  <table id='CourseList' cellSpacing='0'>
+    <thead>
+      <CourseListRow textFirstCell='Available courses' isHeader={true} />
+      <CourseListRow textFirstCell='Course Name' textSecondCell='Credit' isHeader={true} />
+    </thead>
+    <tbody>
+      {(listCourses.length > 0) ? 
+            (listCourses.map(({id, name, credit}) => (
+              <CourseListRow key={id}  textFirstCell={name} textSecondCell={credit} />
+            ))) :
+            (
+              <tr>
+                <td colSpan='2'>
+                  No course available yet
+                </td>
+              </tr>
+            )
+      }
       </tbody>
-    </table>
+  </table>
   );
-}
+};
+
+CourseList.propTypes = {
+listCourses: PropTypes.arrayOf(CourseShape)
+};
+
+CourseList.defaultProps = {
+listCourses: []
+};
 
 export default CourseList;
