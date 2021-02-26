b4257d4aa10c6a86747a87b7215cdcdf6fd77c05
jorgechauxjr <chauxjorge@hotmail.com>
2021-02-26 13:49:23 -0500

    Mod App.js add import CourseList to app.js

--- 0x03-react_props/task_4/dashboard/src/App/App.js
+++ 0x03-react_props/task_4/dashboard/src/App/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Notifications from '../Notifications/Notifications'
 import Header from '../Header/Header'
 import Login from '../Login/Login'
+import CourseList from '../CourseList/CourseList'
 import Footer from '../Footer/Footer'
 import PropTypes from 'prop-types'; // ES6
 
