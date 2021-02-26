ab503f1d61877c58f1240cd613644714b3e31c5d
jorgechauxjr <chauxjorge@hotmail.com>
2021-02-26 17:27:18 -0500

    Add NotificationItemShape.js

---
+++ 0x03-react_props/task_5/dashboard/src/Notifications/NotificationItemShape.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PropTypes from 'prop-types'; // ES6
+
+const NotificationItemShape = PropTypes.shape({
+  id: PropTypes.number.isRequired,
+  html: PropTypes.shape({ __html: PropTypes.string}),
+  type: PropTypes.string.isRequired,
+  value: PropTypes.string
+});
+
+export default NotificationItemShape;
