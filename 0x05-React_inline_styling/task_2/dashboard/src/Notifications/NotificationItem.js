0a4fe6fb6225dc5a3fc7d5d6a1842652c727ab80
jorgechauxjr <chauxjorge@hotmail.com>
2021-03-08 18:21:32 -0500

    Modify NotificationItem.js inline style task_2

--- 0x05-React_inline_styling/task_2/dashboard/src/Notifications/NotificationItem.js
+++ 0x05-React_inline_styling/task_2/dashboard/src/Notifications/NotificationItem.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'; // ES6
-import './Notifications.css'
+import { StyleSheet, css } from 'aphrodite';
 
 class NotificationItem extends React.PureComponent {
   render() {
-    if (this.props.value) return (<li data-notification-type={this.props.type} onClick={() => { this.props.markAsRead(this.props.id) }}>{this.props.value}</li>);
-    else return (<li data-notification-type={this.props.type} dangerouslySetInnerHTML={this.props.html} onClick={() => { this.props.markAsRead(this.props.id) }}></li>);
+    if (this.props.value) return (<li data-notification-type={this.props.type} onClick={() => { this.props.markAsRead(this.props.id) }} className={css(this.props.type === 'urgent' ? style.urgent : style.default)}>{this.props.value}</li>);
+    else return (<li data-notification-type={this.props.type} dangerouslySetInnerHTML={this.props.html} onClick={() => { this.props.markAsRead(this.props.id) }} className={css(this.props.type === 'urgent' ? style.urgent : style.default)}></li>);
   }
 }
 
@@ -23,5 +23,14 @@ NotificationItem.defaultProps = {
   html: {},
   markAsRead: () => void(0)
 }
+
+const style = StyleSheet.create({
+  default: {
+    color: '#0000ff',
+  },
+  urgent: {
+    color: '#ff0000',
+  }
+});
 
 export default NotificationItem;
