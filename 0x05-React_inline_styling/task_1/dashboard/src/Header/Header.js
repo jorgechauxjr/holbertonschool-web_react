051e893a97185473671d1e3c490eaec379432064
jorgechauxjr <chauxjorge@hotmail.com>
2021-03-08 17:08:35 -0500

    Modify Header.js inline style task_!

--- 0x05-React_inline_styling/task_1/dashboard/src/Header/Header.js
+++ 0x05-React_inline_styling/task_1/dashboard/src/Header/Header.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import logo from '../assets/holbertonlogo.jpg';
-import './Header.css';
+import { StyleSheet, css } from 'aphrodite';
 
 function Header() {
   return (
-    <header className='App-header'>
-      <img src={logo} className='App-logo' alt='logo' />
-      <h1>School dashboard</h1>
+    <header className={css(style.appHeader)}>
+      <img src={logo} className={css(style.appLogo)} alt='logo' />
+      <h1 className={css(style.appHeaderH1)}>School dashboard</h1>
     </header>
   )
 }
+
+const style = StyleSheet.create({
+  appHeader: {
+    backgroundColor: '#fff',
+    borderBottom: '3px solid #e1354b',
+    height: '200px',
+  },
+  appLogo: {
+    width: '200px',
+    height: '200px',
+  },
+  appHeaderH1: {
+    display: 'inline',
+    position: 'relative',
+    top: '-6rem',
+    color: '#e1354b',
+  }
+});
 
 export default Header;
