0bb4e932f6178185e946580915d19d10d6f528c3
jorgechauxjr <chauxjorge@hotmail.com>
2021-03-08 18:32:39 -0500

    Modify CourseListRow.js inline style task_2

--- 0x05-React_inline_styling/task_2/dashboard/src/CourseList/CourseListRow.js
+++ 0x05-React_inline_styling/task_2/dashboard/src/CourseList/CourseListRow.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types'; // ES6
-import './CourseList.css';
+import { StyleSheet, css } from 'aphrodite';
 
 function CourseListRow({ isHeader, textFirstCell, textSecondCell }) {
   let trContent = '';
   let bodyStyle = { backgroundColor: '#f5f5f5ab' };
   let headerStyle = { backgroundColor: '#deb5b545' };
-  const style = isHeader ? headerStyle : bodyStyle;
+  const styleInLine = isHeader ? headerStyle : bodyStyle;
 
   if (isHeader) {
     if (textSecondCell === null) {
-      trContent = (<th colSpan='2'>{textFirstCell}</th>);
+      trContent = (<th colSpan='2' className={css(style.thFirt, style.thTd)}>{textFirstCell}</th>);
     }
     else {
-      trContent = (<React.Fragment><th>{textFirstCell}</th><th>{textSecondCell}</th></React.Fragment>);
+      trContent = (<React.Fragment>
+        <th className={css(style.thTd)}>{textFirstCell}</th>
+        <th className={css(style.thTd)}>{textSecondCell}</th></React.Fragment>);
     }
   } else {
-    trContent = (<React.Fragment><td>{textFirstCell}</td><td>{textSecondCell}</td></React.Fragment>);
+    trContent = (<React.Fragment>
+      <td className={css(style.thTd)}>{textFirstCell}</td>
+      <td className={css(style.thTd)}>{textSecondCell}</td></React.Fragment>);
   }
 
-  return (<tr style={style}>{trContent}</tr>);
+  return (<tr style={styleInLine}>{trContent}</tr>);
 }
 
 CourseListRow.propTypes = {
@@ -32,5 +36,15 @@ CourseListRow.defaultProps = {
   isHeader: false,
   textSecondCell: null
 };
+
+const style = StyleSheet.create({
+  thFirt: {
+    textAlign: 'center',
+  },
+  thTd: {
+    padding: '0.25rem',
+    border: '1px solid lightgray',
+  }
+});
 
 export default CourseListRow;
